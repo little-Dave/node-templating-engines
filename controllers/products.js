2d1a6688a72218f7eac51fe30d4d10abc546b0b6
little-Dave <17536424+little-Dave@users.noreply.github.com>
2020-07-19 19:55:01 -0600

    migrate GET / logic from routes/shop.js to controllers/products.js
    update logic to reference products array available in products.js

--- controllers/products.js
+++ controllers/products.js
@@ -14,3 +14,14 @@ exports.postAddProduct = (req, res, next) => {
   products.push({title: req.body.title});
   res.redirect('/');
 }
+
+exports.getProducts = (req, res, next) => {
+  res.render("shop", {
+    products: products, 
+    docTitle: "Shop", 
+    path: "/",
+    hasProducts: !!(products.length),
+    shop: true,
+    productCSS: true
+  });
+}
--- routes/shop.js
+++ routes/shop.js
@@ -1,23 +1,9 @@
-const path = require('path');
-
 const express = require('express');
 
-const rootDir = require('../util/path');
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-const adminData = require('./admin');
-
-router.get('/', (req, res, next) => {
-  const products = adminData.products;
-  res.render("shop", {
-    products: products, 
-    docTitle: "Shop", 
-    path: "/",
-    hasProducts: !!(products.length),
-    shop: true,
-    productCSS: true
-  });
-});
+router.get('/', productsController.getProducts);
 
 module.exports = router;
