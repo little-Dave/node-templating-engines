264a6b1f89d94cc3c858b974250eca1d580c955d
little-Dave <17536424+little-Dave@users.noreply.github.com>
2020-09-23 22:59:47 -0600

    add routes to routes/admin and routes/shop
    supply routes for new nav links

--- routes/admin.js
+++ routes/admin.js
@@ -6,6 +6,8 @@ const router = express.Router();
 
 router.get('/add-product', productsController.getAddProduct);
 
+router.get('/products');
+
 router.post('/add-product', productsController.postAddProduct);
 
 module.exports = router;
