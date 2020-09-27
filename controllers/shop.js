16ff2101eb25677d214b90819fcaf2ae9aacdbe3
little-Dave <17536424+little-Dave@users.noreply.github.com>
2020-09-27 12:05:57 -0600

    update shop controller
    correct getProducts method's path and update its docTitle;
    add getCart method;
    comment out the code that is relevant only if using handlebars

--- controllers/shop.js
+++ controllers/shop.js
@@ -4,11 +4,11 @@ exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
     res.render("shop/product-list", {
       products: products, 
-      docTitle: "Shop", 
-      path: "/",
-      hasProducts: !!(products.length),
-      shop: true,
-      productCSS: true
+      docTitle: "All Products", 
+      path: "/products"
+      // hasProducts: !!(products.length),
+      // shop: true,
+      // productCSS: true
     });
   });
 }
@@ -18,10 +18,17 @@ exports.getIndex = (req, res, next) => {
     res.render("shop/index", {
       products: products, 
       docTitle: "Shop", 
-      path: "/",
-      hasProducts: !!(products.length),
-      shop: true,
-      productCSS: true
+      path: "/"
+      // hasProducts: !!(products.length),
+      // shop: true,
+      // productCSS: true
     });
   });
+}
+
+exports.getCart = (req, res, next) => {
+  res.render("shop/cart", {
+    path: "/cart",
+    docTitle: "Your Cart"
+  })
 }
